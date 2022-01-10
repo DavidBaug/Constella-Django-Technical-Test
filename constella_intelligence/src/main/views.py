f32c4aa556a4078e8587423c7402478609297b6d
Jesús Espínola Conde <jesusespinolaconde@gmail.com>
2022-01-10 12:34:38 +0100

    Test realizado

--- constella_intelligence/src/main/views.py
+++ constella_intelligence/src/main/views.py
@@ -1,4 +1,18 @@
 from django.views import generic
+from accounts.models import Comment
+from accounts.forms import CommentForm
 
 class HomePage(generic.TemplateView):
     template_name = "home.html"
+
+class CommentsPage(generic.TemplateView):
+    model = Comment
+    #form_class = PostForm
+    template_name = "comments.html"
+    fields = '__all__'
+
+class AddComment(generic.TemplateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "add_comment.html"
+    #fields = '__all__'
